8d33deec6aacbb77c7d425dd309b395d0a45a3ee
adylanrff <adylan.dev@gmail.com>
2025-11-30 14:31:29 +0100

    feat: [Chapter 4] finish safe queue and add tests using gtest;

--- internal/queue/safe_queue.h
+++ internal/queue/safe_queue.h
@@ -1,16 +1,21 @@
 #pragma once
 
+#include <algorithm>
+#include <condition_variable>
 #include <mutex>
 #include <queue>
+#include <utility>
 
 template <typename T> class SafeQueue {
 private:
   std::queue<T> queue;
   std::mutex mutex;
 
+  std::condition_variable cv;
+
 public:
-  SafeQueue<T>();
-  ~SafeQueue<T>() = default;
+  SafeQueue();
+  ~SafeQueue() = default;
 
   void push(T val);
   bool try_pop(T &out);
@@ -20,21 +25,26 @@ public:
 template <typename T> SafeQueue<T>::SafeQueue() : queue(), mutex(){};
 
 template <typename T> void SafeQueue<T>::push(T val) {
-  std::lock_guard<std::mutex> guard(mutex);
-  queue.push(val);
+  std::lock_guard guard(mutex);
+  queue.push(std::move(val));
+  cv.notify_one();
 };
 
 template <typename T> bool SafeQueue<T>::try_pop(T &out) {
-  std::lock_guard<std::mutex> guard(mutex);
+  std::lock_guard guard(mutex);
   if (queue.empty()) {
     return false;
   }
 
-  out = queue.pop();
+  out = std::move(queue.front());
+  queue.pop();
   return true;
 };
 
 template <typename T> void SafeQueue<T>::wait_and_pop(T &out) {
-  std::lock_guard<std::mutex> guard(mutex);
-  out = queue.pop();
+  std::unique_lock ul(mutex);
+  cv.wait(ul, [this] { return !queue.empty(); });
+
+  out = std::move(queue.front());
+  queue.pop();
 };
